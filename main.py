edf6e124219a2b36be0346cca3ed305d2c7dbf80
mrtaalebi <the.doors.are.locked@gmail.com>
2021-05-06 23:23:04 +0430

    Use weighted random based on number of messages

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 import os
 import asyncio
+import math
 import random
+from pprint import pprint as pp
 
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
@@ -29,16 +31,42 @@ async def connect():
 async def run():
     client = await connect()
 
-    chats_iter, users = client.iter_dialogs(), []
-    count = 0
-    async for chat in chats_iter:
-        count += 1
+    chats, users, accumulative_count = client.iter_dialogs(), [], 0
+    async for chat in chats:
         if chat.is_user:
-            users.append(chat)
-    print(f'From {count} total chats, {len(users)} are PVs and')
-    choosen = random.choice(users)
-    chat = await client.get_entity(choosen.id)
-    print(f'The choosen one is: {chat.first_name} {chat.last_name} - @{chat.username}')
+            user = await client.get_entity(chat.id)
+            if user.username and user.username.endswith('bot'):
+                continue
+            messages = client.iter_messages(user, search='+')
+            async for message in messages:
+                break
+            log_count = int(math.log(messages.total))
+            accumulative_count += log_count
+            users.append((user, messages.total, accumulative_count,))
+            print('.', end='', flush=True)
+
+    puser = lambda user: (
+        f'{"@" + user.username if user.username is not None else "":<32s} '
+        f'{user.first_name} '
+        f'{user.last_name if user.last_name is not None else ""}'
+    )
+
+    print('\n\nYour top 100 PVs are listed:')
+    print('\n'.join([
+        f'[{i+1:04d}] [{count:07d}] {puser(user)}'
+        for i, (user, count, _) in enumerate(sorted(users, key=lambda x: x[1], reverse=True)[0:100])
+    ]))
+
+    print(f'You have {chats.total} total chats, {len(users)} are PVs with {sum([u[1] for u in users])} total messages.')
+    next_ = True
+    while (next_):
+        choosen_num = random.randint(0, accumulative_count)
+        for user, _, count in users:
+            if choosen_num <= count:
+                choosen = user
+                break
+        print(f'The choosen one is: {puser(choosen)}')
+        next_ = input('\nGet another?[Yn] ') not in ['n', 'N']
 
 
 if api_id == 0 or api_hash == "" or phone_number == 0:
